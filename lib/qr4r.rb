807a2747520c7927f29ff50a7c1f291c063a6b56
Mr Rogers <jon@bunnymatic.com>
2012-03-18 16:27:42 -0700

    * improve handling of longer strings

--- lib/qr4r.rb
+++ lib/qr4r.rb
@@ -1,8 +1,33 @@
 require 'rqrcode'
 require 'mojo_magick'
 module Qr4r
-  # size should indicate dot pixel size, size of 3 makes black pixels of 3x3
-  def self.encode(str, outfile, *opts)
+
+  # params we pass to QRCode include :size and :level
+  #   size   - the size of the qrcode (default 4)
+  #   level  - the error correction level, can be:
+  #      * Level :l 7%  of code can be restored
+  #      * Level :m 15% of code can be restored
+  #      * Level :q 25% of code can be restored
+  #      * Level :h 30% of code can be restored
+  # note: if size is not included and 4 appears to be too small for the included string, we'll make it bigger
+  #       if you include size, we'll use it, which may lead to an error if the string is too long
+  # Limitations are as follows:
+  #   size = 1, max string length = 7
+  #   size = 2, max string length = 14
+  #   size = 3, max string length = 24
+  #   size = 4, max string length = 34
+  #   size = 5, max string length = 44
+  #   size = 6, max string length = 58
+  #   size = 7, max string length = 64
+  #   size = 8, max string length = 84
+  #   size = 9, max string length = 98
+  #   size = 10, max string length = 119
+
+  def self.encode(str, outfile, *rest)
+    opts = rest[0] if rest && rest.length > 0
+    if !opts || (opts && !opts[:size])
+      opts = {}.merge(opts || {}).merge({:size => compute_size(str)})
+    end
     qr = RQRCode::QRCode.new(str, opts)
     data = []
     qr.modules.each_index do |x|
@@ -20,5 +45,23 @@ module Qr4r
       c.file outfile
     end
   end
-   
+
+  private
+  SIZE_RESTRICTIONS = [0, 7, 14, 24, 34, 44, 58, 64, 84, 98, 119] 
+
+  def self.compute_size(str)
+    slen = str.size
+    size = 4
+    ii = 0
+    while ii < SIZE_RESTRICTIONS.length do
+      if slen < SIZE_RESTRICTIONS[ii]
+        break
+      end
+      ii+=1
+    end
+    if ii > 10
+      raise "Your string is too big for this encoder.  It should be less than #{SIZE_RESTRICTIONS.last} characters"
+    end
+    return ii
+  end
 end
--- test/qr4r_test.rb
+++ test/qr4r_test.rb
@@ -10,23 +10,73 @@ class Qr4rTest < Test::Unit::TestCase
     # assert that it worked
     assert File.exists?(f.path)
     r = MojoMagick::get_image_size(f.path)
-    assert r[:height] == 99
-    assert r[:width] == 99
+    assert r[:height] == 25
+    assert r[:width] == 25
   end
 
   def test_encode_with_size
     # do something
     f = Tempfile.new(['qr4r','.png'])
-    Qr4r::encode('whatever yo', f.path, 4)
+    Qr4r::encode('whatever yo', f.path, :size => 4)
     # assert that it worked
     assert File.exists?(f)
     r = MojoMagick::get_image_size(f.path)
-    assert r[:height] == 4 * 33
-    assert r[:width] == 4 * 33
+    assert r[:height] == 33
+    assert r[:width] == 33
   end
 
-  def test_a_long_string
+  def test_encode_with_size_and_level
+    # do something
     f = Tempfile.new(['qr4r','.png'])
-    Qr4r::encode('this string should also be encodable. don\'t ya think', f.path, 4)
+    Qr4r::encode('whatever yo', f.path, :size => 4, :level => :m)
+    # assert that it worked
+    assert File.exists?(f)
+    r = MojoMagick::get_image_size(f.path)
+    assert r[:height] == 33
+    assert r[:width] == 33
   end
+
+  def test_a_long_string_with_size_thats_too_small
+    caught = false
+    begin
+      f = Tempfile.new(['qr4r','.png'])
+      Qr4r::encode('this string should also be encodable. don\'t ya think', f.path, :size => 4)
+    rescue
+      caught = true
+    end
+    assert caught, 'Expected an error'
+  end
+
+  def test_a_long_string_with_size_thats_right
+    f = Tempfile.new(['qr4r','.png'])
+    Qr4r::encode('this string should also be encodable. don\'t ya think', f.path, :size => 10)
+  end
+
+  def test_a_long_string_without_size
+    f = Tempfile.new(['qr4r','.png'])
+    Qr4r::encode('this string should also be encodable. don\'t ya think', f.path, :size => 10)
+  end
+
+  def test_compute_size
+    test_sizes =   [ 7, 14, 24, 34, 44, 58, 64, 84, 98]
+    test_sizes.each_with_index do |sz, idx|
+      str = 'a'*(sz-1)
+      assert Qr4r.send(:compute_size, str) == idx+1
+      str = 'a'*(sz)
+      assert Qr4r.send(:compute_size, str) == idx+2
+    end
+  end
+
+  def test_compute_size_too_big
+    str = 'a'*120
+    caught = false
+    begin
+      Qr4r.send(:compute_size, str)
+    rescue Exception => ex
+      caught = true
+    end
+    assert caught, "Expected exception"
+    
+  end
+    
 end

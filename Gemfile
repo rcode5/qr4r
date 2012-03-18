d20efdbe598ac0bc9c5533a98b5a2a28b7d5e60b
Mr Rogers <jon@bunnymatic.com>
2012-03-17 23:46:17 -0700

    * update to test on 1.8.7  * added rake to dev group on gemfile

--- Gemfile
+++ Gemfile
@@ -3,3 +3,6 @@ source :rubygems
 gem 'rqrcode'
 gem 'mojo_magick', :path => '/projects/mojo_magick'
 
+group :test, :development do 
+  gem 'rake'
+end
--- lib/qr4r.rb
+++ lib/qr4r.rb
@@ -14,7 +14,8 @@ module Qr4r
       end
     end
     MojoMagick::convert do |c|
-      c.blob (data.pack('C'*data.length)), :format => :rgb, :depth => 8, :size => "%dx%d" % [qr.modules.size, qr.modules.size]
+      d = data.pack('C'*data.size.to_i)
+      c.blob(data.pack(d, :format => :rgb, :depth => 8, :size => ("%dx%d" % [qr.modules.size, qr.modules.size])))
       c.file outfile
     end
   end
